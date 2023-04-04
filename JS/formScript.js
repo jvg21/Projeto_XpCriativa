ee6579d7255f142a8027d2a46358938fadcf7d2c
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-04-04 14:19:43 -0300

    form_action
    mostra campos do formulario em outra página html

--- JS/formScript.js
+++ JS/formScript.js
@@ -1,3 +1,27 @@
+function getParameters() {
+
+  var params    = new Array();
+  var paramsRet = new Array();
+  var url = window.location.href;     // Obtém a URL
+  var paramsStart = url.indexOf("?"); // Procura ? na URL
+
+  if(paramsStart != -1){ 
+   // Encontrou ? na URL
+    var paramString = url.substring(paramsStart + 1); // Retorna parte da URL após ?
+    paramString = decodeURIComponent(paramString);    // Decodifica código de URI da URL
+    var params = paramString.split("&"); // Retorna trechos da String separados por &
+    for(var i = 0 ; i < params.length ; i++) {
+      var pairArray = params[i].split("="); // Retorna trechos da String separados por =
+      if(pairArray.length == 2){
+        paramsRet[pairArray[0]] = pairArray[1];
+      }
+
+    }
+    return paramsRet;
+  }
+  return null; // Não encontrou ? na URL
+}
+
 function MostrarSenha(id){
     var campo = document.getElementById(id);
     if(campo.type == "password"){
