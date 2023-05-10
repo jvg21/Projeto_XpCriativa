0e1352e13d49b862c3039b77b0ece1cf23ba5206
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-05-10 02:55:43 -0300

    cardschati

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Hotelzin</title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="estilo/style.css">
+    <!-- <link rel="stylesheet" href="estilo/rl.css"> -->
     <script src="JS/func.js" defer></script>
    
 </head>
@@ -18,31 +19,8 @@
             <p><mark>Os melhores quartos com os melhores preços.</mark></p>
         </div>
     </main>
-        
-    <div class="container">
-        <div class="card red">
-          <img src="imagens/quarto1.jpg" h2>
-          <h2>Quartos Padrões</h2>
-          <p>A partir de R$ 324,00</p>
-          <button class="button" onclick="roomList()">Reserve aqui</button>
-        </div>
-        
-        <div class="card white">
-          <img src="imagens/quarto 2.jpg" h2>
-          <h2>Quartos com 2 suítes</h2>
-          <p>A partir de R$ 548,00</p>
-          <button class="button">Reserve aqui</button>
-        </div>
-        
-        <div class="card red">
-          <img src="imagens/quarto3.jpg" h2>
-          <h2>Quartos de luxo</h2>
-          <p>A partir de R$ 926,00</p>
-          <button class="button" href='http://localhost/xp/Projeto_XpCriativa/RoomCrud/roomList.php'>Reserve aqui</button>
-        </div>
-      </div>
-      
 
+    <?php include 'rl.html'?>
       <?php include './geral/rodape.html'?>
 </body>
 </html>
