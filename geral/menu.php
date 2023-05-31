56eec679f8a5c0ea3dee1914c6779cd43a350fc0
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-05-31 02:25:21 -0300

    menu atualizado

--- geral/menu.php
+++ geral/menu.php
@@ -1,55 +1,58 @@
-<header class='menu-pagina'>
-    
-    <!-- PARTE INICIAL DE CIMA, OQUE ESTÁ EM VERMELHO -->
-    <!-- <link rel="stylesheet" href="estilo/estiloPagina.css"> -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.min.js" defer></script>
-    <link rel="stylesheet" href="http://localhost/xp/Projeto_XpCriativa/estilo/estiloPagina.css">
-    <img src="http://localhost/xp/Projeto_XpCriativa/imagens/logo3.png">
-    <div class="container-menu">
-        <!-- <div class="logo">
-            <h1><a href="#">Hotelzin</a></h1>
-            <img src="imagens/hel.PNG">
-        </div> --->
-        <nav class="nav-menu">
-            <ul>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-                <li><a href="http://localhost/xp/Projeto_XpCriativa/">Home</a></li>
-            </ul>
-        </nav>
-       
-    </div>
-    <div class="menu-user">
-    <?php
-    include 'controle.php';
-            
-            if(!isset($_SESSION ['login'])){ 
-                // if(isset($_SESSION['Pagina_Controlada'])==true){
-                //     unset($_SESSION['Pagina_Controlada']);
-                //     header("location: ./index.php");
-                // }
-                echo'<nav class="nav-menu"><ul>
-                <li><a type="button"  href="./CadUser.php" class="btn btn-success btn-block">Cadastrar</a></li>
-                <li><a type="button" href="./login.php" class="btn btn-success btn-block">Login</a></li>
-                </ul></nav>';
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Menu</title>
+    <link rel="stylesheet" href="estilo/menuCss.css">
+</head>
+<body>
 
-            }else{
-                echo'<nav class="nav-menu"><ul>';
-                if($_SESSION['nivel'] == "ADM"){
-                    echo '<li><a type="button" href="http://localhost/xp/Projeto_XpCriativa/ADM/menuAdm.php" class="btn btn-success btn-block">MENU ADM</a></li>';
-
-                }
-                echo'
-                <li><a type="button"  href="./AltUser.php" class="btn btn-success btn-block">Alterar Dados</a></li>
-                <li><a type="button"  href="./AltSenha.php" class="btn btn-success btn-block">Trocar Senha</a></li>
-                <li><a type="button" href="http://localhost/xp/Projeto_XpCriativa/login/logout.php" class="btn btn-success btn-block">Logout</a></li>
-                <li>Bem Vindo '.$_SESSION['nivel'].': '.$_SESSION['nome'].'</li>
-                </ul> </nav>';
-            }
+    <nav>
         
-        ?>
-    </div>
-</header>
+        <ul>
+            
+            <?php
+            include 'controle.php';
+                if(!isset($_SESSION ['login'])){ 
+                    echo '<li><a href="http://localhost/xp/Projeto_XpCriativa/login.php" class="btn btn-outline-light">Login</a></li>';
+                    echo '<li><a href="http://localhost/xp/Projeto_XpCriativa/CadUser.php" class="btn btn-outline-light">Cadastre-se</a></li.';
+                }else{
+                    echo'<li>
+                            <div>
+                                <i class="fas fa-user usuario-logado"></i>
+                                <span>Bem Vindo '.$_SESSION['nivel'].', '.$_SESSION["nome"].'</span>
+                            </div>
+                        </li>
+                    <li class="dropdown">
+                        <a class="menu-drop">☰</a>
+                        <div class="dropdown-menu">';
+                        if($_SESSION['nivel'] == 'ADM'){
+                            echo'
+                            <a href="http://localhost/xp/Projeto_XpCriativa/Adm/menuAdm.php">Menu Adm</a>';
+                            
+                        }//else{
+                            
+                        // }
+
+                            
+                        echo'
+                        <a href="http://localhost/xp/Projeto_XpCriativa/login/AltUser.php">Meus Dados</a>
+                        <a href="http://localhost/xp/Projeto_XpCriativa/login/AltSenha.php">Trocar Senha</a>
+                        <a href="http://localhost/xp/Projeto_XpCriativa/login/logout.php">Logout</a>
+                        </div>
+                    </li>';
+                }
+            ?>
+
+            
+            
+        </ul>
+    </nav>
+    
+</body>
+</html>
+
---
+++ geral/menuVELHO.php
@@ -0,0 +1,55 @@
+<header class='menu-pagina'>
+    
+    <!-- PARTE INICIAL DE CIMA, OQUE ESTÁ EM VERMELHO -->
+    <!-- <link rel="stylesheet" href="estilo/estiloPagina.css"> -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.min.js" defer></script>
+    <link rel="stylesheet" href="http://localhost/xp/Projeto_XpCriativa/estilo/estiloPagina.css">
+    <img src="http://localhost/xp/Projeto_XpCriativa/imagens/logo3.png">
+    <div class="container-menu">
+        <!-- <div class="logo">
+            <h1><a href="#">Hotelzin</a></h1>
+            <img src="imagens/hel.PNG">
+        </div> --->
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="http://localhost/xp/Projeto_XpCriativa/">Home</a></li>
+            </ul>
+        </nav>
+       
+    </div>
+    <div class="menu-user">
+    <?php
+    include 'controle.php';
+            
+            if(!isset($_SESSION ['login'])){ 
+                // if(isset($_SESSION['Pagina_Controlada'])==true){
+                //     unset($_SESSION['Pagina_Controlada']);
+                //     header("location: ./index.php");
+                // }
+                echo'<nav class="nav-menu"><ul>
+                <li><a type="button"  href="./CadUser.php" class="btn btn-success btn-block">Cadastrar</a></li>
+                <li><a type="button" href="./login.php" class="btn btn-success btn-block">Login</a></li>
+                </ul></nav>';
+
+            }else{
+                echo'<nav class="nav-menu"><ul>';
+                if($_SESSION['nivel'] == "ADM"){
+                    echo '<li><a type="button" href="http://localhost/xp/Projeto_XpCriativa/ADM/menuAdm.php" class="btn btn-success btn-block">MENU ADM</a></li>';
+
+                }
+                echo'
+                <li><a type="button"  href="./AltUser.php" class="btn btn-success btn-block">Alterar Dados</a></li>
+                <li><a type="button"  href="./AltSenha.php" class="btn btn-success btn-block">Trocar Senha</a></li>
+                <li><a type="button" href="http://localhost/xp/Projeto_XpCriativa/login/logout.php" class="btn btn-success btn-block">Logout</a></li>
+                <li>Bem Vindo '.$_SESSION['nivel'].': '.$_SESSION['nome'].'</li>
+                </ul> </nav>';
+            }
+        
+        ?>
+    </div>
+</header>
