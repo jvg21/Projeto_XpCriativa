89bff4e73a0c985f4d184f222d9c6cea970859ef
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-05-30 23:51:57 -0300

    oi

--- Adm/menuAdm.php
+++ Adm/menuAdm.php
@@ -13,13 +13,20 @@
     <?php
         include '../geral/menu.php';
         redirect_if_not_adm();?>
+ 
+    <div class="Nav">
+    
+        <li><a href="#quartos" onclick="trocarCrud()">Quartos</a></li>    
+        <li><a href="#ModeloQuartos" onclick="trocarCrud2()">Modelo de Quartos</a></li>
+    </div>
     
     <div class="container">
         
-        <div class="lista-quartos">
+        
+        <div class="lista-quartos" id="lista-quartos">
             <?php include 'Quarto/roomList.php';?>
         </div>
-        <div class="lista-tipo-quartos">
+        <div class="lista-tipo-quartos" id="lista-tipo-quartos">
             <?php include 'TipoQuarto/TroomList.php';?>
         </div>
 
--- JS/crudAdm.js
+++ JS/crudAdm.js
@@ -1,3 +1,11 @@
 function voltarMenuAdm(){
     window.location.replace('../menuAdm.php');
 }
+
+function trocarCrud2(){
+    //alert('chato');
+    if(document.getElementById('lista-tipo-quartos').style.display == 'none'){
+        document.getElementById('lista-quartos').style.display = 'none';
+        document.getElementById('lista-tipo-quartos').style.display='block';
+    }
+}
