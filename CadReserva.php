76daaa25ee6a1c3a111e68b09afbf4346e5d8b4b
Giovanni Lucca Reque Uchoa <gi.lucca10@gmail.com>
2023-06-06 19:01:06 -0300

    including reservation files
    giovanni uchoa

--- CadReserva.php
+++ CadReserva.php
@@ -6,32 +6,53 @@
     <link rel="stylesheet" href="estilo/style.css">
     <link rel="stylesheet" href="estilo/form.css">
     <script src="JS/formScript.js" defer></script>
-    <title>Hotelzin - Agendar Quarto</title>
-    
+    <title>Hotelzin - Reserva</title>
 </head>
 <body>
-
-    <?php
-    include './geral/menu.php';
-    redirect_if_not_login();
-    ?>
-
+    <?php include './geral/menu.php'?>
+    
     <div class="container">
-        <form class="form-signup" action="">
-            <h2 class="h2-form">Agende sua hospedagem!</h2><br>
-            <input type="date" placeholder="Data de entrada" class="form-control input-reserva">
-            <input type="time" placeholder="Hora entrada" class="form-control input-reserva">
-            <select name="" id="" class="form-control select-reserva">
-                <option value="" disabled selected>Selecione um Quarto</option>
-                <option value="">Single</option>
-                <option value="">Couple</option>
-                <option value="">Premium</option>
-            </select>
-            <input type="date" placeholder="Data de saída" class="form-control input-reserva">
-            <input type="time" placeholder="Hora saída" class="form-control input-reserva">
-            <input type="submit" value="Agendar" class="btn btn-primary">
+        <form class="form-signup" action="reservaCrud/ReservaExe.php" method="POST" enctype="multipart/form-data">
+            <h2>Reservar Quarto</h2>
+            <div class="form-group mb-2">
+                <select name="quarto" required  class="form-select">
+                    <option value="">Quarto*</option>
+                    <option value="1">Single</option>
+                    <option value="2">Dual</option>
+                    <option value="3">Medium</option>
+                    <option value="4">Premium</option>
+                </select>              
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="data_in">Data Check-In* </label>
+                <input type="date" class="form-control" id="data_in" name="data_in" required autocomplete="off"
+                pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}" >
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="data_out">Data Check-Out* </label>
+                <input type="date" class="form-control" id="data_out" name="data_out" required autocomplete="off"
+                pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}" >
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="hora_in">Hora Check-In* </label>
+                <input type="time" class="form-control" id="hora_in" name="hora_in" required autocomplete="off"  >
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="hora_out">Hora Check-Out* </label>
+                <input type="time" class="form-control" id="hora_out" name="hora_out" required autocomplete="off"  >
+            </div>
+
+            <input type="submit" class="btn btn-success btn-block" name="" value="Enviar" >
         </form>
     </div>
+     
     <?php include './geral/rodape.html'?>
+    
+            
+    
 </body>
 </html>
