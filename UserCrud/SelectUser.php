d667c62b1c644f273989be54bc4e15597b5262a1
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-06-07 15:49:48 -0300

    Create SelectUser.php

---
+++ UserCrud/SelectUser.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/style.css">
+    <link rel="stylesheet" href="estilo/form.css">
+    
+    <script src="JS/formScript.js" defer></script>
+    <title>Hotelzin - Cadastro</title>
+</head>
+<body>
+    <?php include '../geral/menu.php';
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ?>
+
+</body>
+<html>
