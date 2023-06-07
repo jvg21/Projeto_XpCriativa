ff73379f366250b820f0a7fdefa108e50b7678b0
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-06-07 15:41:17 -0300

    Update InsertUser.php

--- UserCrud/InsertUser.php
+++ UserCrud/InsertUser.php
@@ -1,14 +1,15 @@
 <?php 
 session_start();
-require '../BD/ConectaDB.php';
-$conn = new mysqli($LOCALDB, $USER, $PASS, $DATABASE);
+require '../BD/ConectaDB.php';//**ARQUIVO NESCESSÁRIO PARA CONEXÕES NO BD
+$conn = new mysqli($LOCALDB, $USER, $PASS, $DATABASE); //**CRIA A CONEXÃO NO BD
 
-// Verifica conexão 
+// **Verifica conexão 
 if ($conn->connect_error) {
     die("<strong> Falha de conexão: </strong>" . $conn->connect_error);
 }
 
-try{
+if(isset($_POST["cpf"])){
+    //tag dentro do $_Post é o name do html
     $Nome = $_POST["firstname"]." ".$_POST["lastname"];
     $Cpf = $_POST["cpf"];
     $Email = $_POST["email"];
@@ -16,26 +17,29 @@ try{
     $Sexo = $_POST["Sexo"];
     $Data = $_POST["data_nasc"];
     $Senha = $_POST["password"];
-}catch(Exception $e){
+}
+else{
     include 'http://localhost/xp/Projeto_XpCriativa/geral/controle.php';
     redirect();
 }
 
-
 try{
 
     if ($_FILES['imagem']['size'] == 0) { 
+        //**query banco de dados               //**nome dos campos no banco de dados                          
         $sql= "INSERT INTO $DATABASE.usuario (nome,cpf,email,telefone,data_nasc,sexo,senha,foto) VALUES 
         ('$Nome','$Cpf','$Email','$Telefone','$Data','$Sexo',md5('$Senha'),NULL);";
+
     } else {                             
         $imagem = addslashes(file_get_contents($_FILES['imagem']['tmp_name'])); // Atribui a foto a uma variavel
-        $sql= "INSERT INTO $DATABASE.usuario (nome,cpf,email,telefone,data_nasc,sexo,senha,foto) VALUES 
+        //***************************** */
+        $sql= "INSERT INTO $DATABASE.usuario 
+        (nome,cpf,email,telefone,data_nasc,sexo,senha,foto) VALUES 
         ('$Nome','$Cpf','$Email','$Telefone','$Data','$Sexo',md5('$Senha'),'$imagem');";
     }
 
-    $result = $conn->query($sql);
-    $_SESSION ['login']       = $Email;           // Ativa as variáveis de sessão
-   // $_SESSION ['ID_Usuario']  = $row['idcliente'];
+    $result = $conn->query($sql); //**realiza a query no banco(cadastro)
+    $_SESSION ['login']       = $Email;      
     $_SESSION ['nome']        = $Nome;
     $_SESSION ['nivel']       = "CLIENTE";
 
@@ -49,7 +53,6 @@ try{
 catch(Exception $e) {
     echo $e;
     ?>
-    
     <script language="javascript" type="text/javascript">
         alert("Erro ao cadastrar");
         window.location.href = 'http://localhost/xp/Projeto_XpCriativa/CadUser.php';
