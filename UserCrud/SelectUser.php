0067adb3b60ebcc3b4bc934fdb9a67662a687c91
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-06-08 18:46:08 -0300

    Update SelectUser.php

--- UserCrud/SelectUser.php
+++ UserCrud/SelectUser.php
@@ -12,6 +12,25 @@
 <body>
     <?php include '../geral/menu.php';
     include '../BD/ConectaDB.php';
+    $conn = new mysqli($LOCALDB, $USER, $PASS, $DATABASE);
+    // **Verifica conexão 
+    if ($conn->connect_error) {
+        die("<strong> Falha de conexão: </strong>" . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM ".$DATABASE.".usuario";
+    $result = $conn->query($sql);
+    if ($result->num_rows >0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "Seu nome é:". $row['nome']." ";
+            echo "Nasceu:". $row['data_nasc'];
+            echo "Time do coração é ".$row["Time"];
+            echo '<br>';
+
+        }
+    }
+
+    
     ////fazer o select
     
     
